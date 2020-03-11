afdaf3459ab4bfd570b99ae2014174572050159d
Domenico Cascone <dcascone84@gmail.com>
2020-03-11 15:04:51 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,5 @@
+require "pry"
+
 def reverse_each_word(phrase)
-reversed_words = []
-phrase.split
-  phrase.each do |word|
-  reverse_each_word << word.reverse
-  end
-  reversed_words
+phrase.split.collect { |word| word.reverse }.join(" ")
 end

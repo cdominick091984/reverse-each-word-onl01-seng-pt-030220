5d9c9f7eb5523147f5272374965bf48c1ceb6694
Domenico Cascone <dcascone84@gmail.com>
2020-03-11 15:04:23 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,5 @@
 require "pry"
 
 def reverse_each_word(phrase)
-phrase_in_array = phrase.split
-  phrase_in_array.collect do |word|
-    word.reverse.join(" ")
-
-    #binding.pry
-  end
-
+phrase.split.collect { |word| word.reverse }.join(" ")
 end

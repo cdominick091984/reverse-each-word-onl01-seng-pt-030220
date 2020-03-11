4d24ac873910fe1ae6c2656d63fb785461c9ca02
Domenico Cascone <dcascone84@gmail.com>
2020-03-11 14:56:45 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,8 +3,8 @@ require "pry"
 def reverse_each_word(phrase)
 phrase_in_array = phrase.split
   phrase_in_array.collect do |word|
-    array_in_reverse = word.reverse
-    phrase_in_array << array_in_reverse
+    puts word.reverse
+
     binding.pry
   end
 
